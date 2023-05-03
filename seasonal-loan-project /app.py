793c5d61ab5f990e075646cb147cdd315a658f92
Kendi Rita <ritabkendi@gmail.com-MacBook-Pro.local>
2023-05-04 00:29:15 +0300

    code cleanup.

--- seasonal-loan-project /app.py
+++ seasonal-loan-project /app.py
@@ -1,43 +1,44 @@
+# Import necessary libraries and modules
 from flask import Flask, render_template, request
 from calculation import calculate_loan_schedule
 from datetime import datetime
 
+# Initialize Flask app
 app = Flask(__name__)
+
+# define home route
 
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+# Define route for calculating loan schedule
+
 
 @app.route('/calculate_schedule', methods=['POST'])
 def calculate_schedule():
-    print("success")
-    # extract data from form
 
+    # extract data from form submitted by user
     loan_product = request.form.get('loan_product')
     loan_amount = float(request.form.get('loan_amount'))
     num_months = int(request.form.get('num_months'))
     Expected_Disbursement_Date = request.form.get('Expected_Disbursement_Date')
     Expected_Disbursement_Date = datetime.strptime(
         Expected_Disbursement_Date, '%Y-%m-%d').date()
-
     select_months = request.form.getlist('months')
     periods_per_year = len(select_months)
-
-    # interest = float(request.form['interest'])
-    # admin_fee = 0.35
-    # loan_term = int(request.form['term'])
-    # monthly_due_date = datetime.strptime('2023-05-02', '%Y-%m-%d').date()
     due_date = int(request.form.get('due_date'))
 
-    interest_rate = 10.90
-
+    # calculate loan repayment schedule based on user input
     schedule = calculate_loan_schedule(
         num_months, loan_amount, periods_per_year, Expected_Disbursement_Date, due_date, loan_product, select_months)
 
+    print('AWesome')
+    # render home template with loan schedule data to be displayed to the user
     return render_template('home.html', schedule=schedule)
 
 
+# run Flask app
 if __name__ == "__main__":
     app.run(debug=True)
--- seasonal-loan-project /calculation.py
+++ seasonal-loan-project /calculation.py
@@ -1,13 +1,32 @@
+# import necessary modules
 from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
 import math
 
+# set loan interest rate
 LOAN_INTEREST_RATE = 10.90
 
 
 def calculate_loan_schedule(num_of_months, total_loan, periods_per_year, disbursement_date, due_date, loan_product, select_months):
+    """
+        calculates the repayment schedule for a loan.
+
+        Args:
+                - num_of_months: The number of months for the loan.
+                - total_loan: The total amount of the loan.
+                - periods_per_year: The number of periods (usually months) per year.
+                - disbursement_date: The date the loan is disbursed.
+                - due_date: The due date for the first repayment.
+                - loan_product: The type of loan product.
+                - select_months: The months for which to generate the repayment schedule.
+        return: 
+                - A list of dictionaries representing the repayment schedule, where each dictionary represents a single period.
+    """
+
+    # set processing fees
     processing_fees = 0.02
-    # initialize loan schedule
+
+   # Initialize loan schedule and add processing fee as first item
     schedule = [
         {
             'period': "Fees",
@@ -19,33 +38,37 @@ def calculate_loan_schedule(num_of_months, total_loan, periods_per_year, disburs
         }
     ]
 
-    # total payment period
+    # calculate the total number of payment periods
     payment_periods = (num_of_months / 12) * periods_per_year
-    # pmt initial
-    total_amount = total_loan / payment_periods
-    # print(total_amount)
 
-    # set initial balance to the total amount
+    # calculate the initial payment amount
+    total_amount = total_loan / payment_periods
+
+    # set initial balance to the total_loan amount
     balance = total_loan
 
-    prev_due_date = disbursement_date
-
+    # set the monthly due date to one month after disbursement date
     monthly_due_date = disbursement_date + relativedelta(months=1)
 
+    # Initialize the accrued interest
+    # and current_month paid variables
     accrued_interest = 0
     is_current_month_paid = False
 
+    # calculate the total number of payment periods,
+    # rounded up to the nearest integer
     all_payments_periods = math.ceil(num_of_months / 12) * periods_per_year
+
+    # Calculate the monthly fees a
+    # and the total fees to be paid over all payment periods
     Monthly_fees = 0.0035 * total_loan
     FEES = (Monthly_fees * num_of_months) / all_payments_periods
-    print(FEES)
 
-    # loop through payment periods and add entries to the schedule
+    # Loop through payment periods and add entries to the schedule
     for period in range(1, num_of_months + 1):
-        # number of days since the last payment
 
+        # Calculate the number of days since the last payment
         if period == 1:
-
             monthly_due_date = monthly_due_date.replace(day=due_date)
             days_since_last_payment = (
                 monthly_due_date - disbursement_date).days
@@ -54,33 +77,32 @@ def calculate_loan_schedule(num_of_months, total_loan, periods_per_year, disburs
             days_since_last_payment = (
                 monthly_due_date - (monthly_due_date - relativedelta(months=1))).days
 
+        # Get the current month
         current_month = monthly_due_date.strftime("%B")
 
         # interest due for this period
         interest = (balance * LOAN_INTEREST_RATE *
                     days_since_last_payment)/36000
 
-        # principal due for the period
+        # Calculate principal due for the period
         principal = (total_amount - interest - FEES)
 
-        # add payment to the schedule
-        # schedule.append(payment)
-
-        # print(f"Interest: ", interest, "Balance: ", balance)
-        # print(f"days_since_last_payment, {days_since_last_payment}")
-
+        # Check if current month is in the list of months selected
         if current_month in select_months:
             is_current_month_paid = True
-            # Calculate principal, interest etc
 
+            # Calculate principal and interest due for the current month.
             # TOTAL INTEREST IS C INT + ACCRUED
             interest = interest + accrued_interest
-
             principal = (total_amount - interest - FEES)
+
             # update balance for the next period
             balance -= principal
+
             # NULL THE ACCRUED INTEREST
             accrued_interest = 0
+
+            # Add payment details to the schedule
             schedule.append({
                 'period': period,
                 'date': monthly_due_date,
@@ -90,11 +112,11 @@ def calculate_loan_schedule(num_of_months, total_loan, periods_per_year, disburs
                 'total_amount': total_amount,
             })
 
-            # print(schedule)
-
         else:
             # Add interst to accrued_interest
             accrued_interest = interest + accrued_interest
+
+            # Add an empty payment to the schedule for the current period
             schedule.append({
                 'period': period,
                 'date': monthly_due_date,
@@ -104,8 +126,4 @@ def calculate_loan_schedule(num_of_months, total_loan, periods_per_year, disburs
                 'total_amount': "-",
             })
 
-        # print("Accrued Interest: ", accrued_interest)
-
-        # # update the due date for the next period
-        # prev_due_date = due_date
     return schedule
